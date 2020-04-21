8370131f5b790352a03fe69ea56077d03bf90e70
Michael Lange <Michael.Lange@ecmwf.int>
2020-04-21 16:03:45 +0100

    CLOUDSC-Fortran: Rename V2 and separate validation into utilities lib
    The "modern" Fortran version is now called `dwarf-cloudsc-fortran` and
    the redundant `cloudsc` subdir has been removed.

--- src/cloudsc_fortran/cloudsc_global_state_mod.F90
+++ src/cloudsc_fortran/cloudsc_global_state_mod.F90
@@ -5,6 +5,7 @@ MODULE CLOUDSC_GLOBAL_STATE_MOD
   USE YOECLDP, ONLY: NCLV
   USE LOAD_ARRAY_MOD, ONLY: QUERY_DIMENSIONS, LOAD_AND_EXPAND, LOAD_AND_EXPAND_STATE, &
    &                        INITIALISE_PARAMETERS, FINALIZE
+  USE VALIDATE_MOD, ONLY: VALIDATE
 
   IMPLICIT NONE
 
@@ -86,10 +87,6 @@ MODULE CLOUDSC_GLOBAL_STATE_MOD
   INTERFACE FIELD_INIT
      PROCEDURE FIELD_INIT_R1, FIELD_INIT_R2, FIELD_INIT_R3, FIELD_INIT_STATE
   END INTERFACE FIELD_INIT
-
-  INTERFACE VALIDATE
-     PROCEDURE VALIDATE_R1, VALIDATE_R2, VALIDATE_R3
-  END INTERFACE VALIDATE
 
 CONTAINS
 
@@ -315,148 +312,5 @@ CONTAINS
     CALL VALIDATE('TENDENCY_LOC%CLD', B_LOC(:,:,7:,:), THIS%B_LOC(:,:,7:,:), KLON, THIS%KLEV, NCLV, NGPTOT, NBLOCKS)
 
   END SUBROUTINE CLOUDSC_GLOBAL_STATE_VALIDATE
-
-    SUBROUTINE VALIDATE_R1(NAME, REF, FIELD, NLON, NGPTOT, NBLOCKS)
-    ! Computes and prints errors "in the L1 norm sense"
-    CHARACTER(*), INTENT(IN) :: NAME
-    REAL(KIND=JPRB), INTENT(INOUT) :: REF(:,:), FIELD(:,:)
-    INTEGER(KIND=JPIM), INTENT(IN) :: NLON, NBLOCKS, NGPTOT
-
-    INTEGER :: B, BSIZE, JK
-    REAL(KIND=JPRB) :: ZMINVAL, ZMAXVAL, ZDIFF, ZMAXERR, ZERRSUM, ZSUM, ZRELERR, ZAVGPGP
-
-    ZMINVAL = +HUGE(ZMINVAL)
-    ZMAXVAL = -HUGE(ZMAXVAL)
-    ZMAXERR = 0.0_JPRB
-    ZERRSUM = 0.0_JPRB
-    ZSUM = 0.0_JPRB
-
-    !OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(B, BSIZE) &
-    !& REDUCTION(MIN:ZMINVAL, MAX:ZMAXVAL, MAX:ZMAXERR, +:ZERRSUM, +:ZSUM)
-    DO B=1, NBLOCKS
-       BSIZE = MIN(NLON, NGPTOT - (B-1)*NLON)  ! Field block size
-       ZMINVAL = MIN(ZMINVAL,MINVAL(FIELD(:,B)))
-       ZMAXVAL = MAX(ZMAXVAL,MAXVAL(FIELD(:,B)))
-       DO JK=1, bsize
-          ! Difference against reference result in one-norm sense
-          ZDIFF = ABS(FIELD(JK,B) - REF(JK,B))
-          ZMAXERR = MAX(ZMAXERR,ZDIFF)
-          ZERRSUM = ZERRSUM + ZDIFF
-          ZSUM = ZSUM + ABS(REF(JK,B))
-       END DO
-    END DO
-    ZAVGPGP = ZERRSUM / REAL(NGPTOT,JPRB)
-
-    CALL ERROR_PRINT(NAME, ZMINVAL, ZMAXVAL, ZMAXERR, ZERRSUM, ZSUM, ZAVGPGP, NDIM=1)
-  END SUBROUTINE VALIDATE_R1
-
-  SUBROUTINE VALIDATE_R2(NAME, REF, FIELD, NLON, NLEV, NGPTOT, NBLOCKS)
-    ! Computes and prints errors "in the L1 norm sense"
-    CHARACTER(*), INTENT(IN) :: NAME
-    REAL(KIND=JPRB), INTENT(INOUT) :: REF(:,:,:), FIELD(:,:,:)
-    INTEGER(KIND=JPIM), INTENT(IN) :: NLON, NLEV, NBLOCKS, NGPTOT
-
-    INTEGER :: B, BSIZE, JL, JK
-    REAL(KIND=JPRB) :: ZMINVAL, ZMAXVAL, ZDIFF, ZMAXERR, ZERRSUM, ZSUM, ZRELERR, ZAVGPGP
-
-    ZMINVAL = +HUGE(ZMINVAL)
-    ZMAXVAL = -HUGE(ZMAXVAL)
-    ZMAXERR = 0.0_JPRB
-    ZERRSUM = 0.0_JPRB
-    ZSUM = 0.0_JPRB
-
-    !OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(B, BSIZE) &
-    !& REDUCTION(MIN:ZMINVAL, MAX:ZMAXVAL, MAX:ZMAXERR, +:ZERRSUM, +:ZSUM)
-    DO B=1, NBLOCKS
-       BSIZE = MIN(NLON, NGPTOT - (B-1)*NLON)  ! Field block size
-       ZMINVAL = MIN(ZMINVAL,MINVAL(FIELD(:,:,B)))
-       ZMAXVAL = MAX(ZMAXVAL,MAXVAL(FIELD(:,:,B)))
-       DO JL=1, NLEV
-          DO JK=1, bsize
-             ! Difference against reference result in one-norm sense
-             ZDIFF = ABS(FIELD(JK,JL,B) - REF(JK,JL,B))
-             ZMAXERR = MAX(ZMAXERR,ZDIFF)
-             ZERRSUM = ZERRSUM + ZDIFF
-             ZSUM = ZSUM + ABS(REF(JK,JL,B))
-          ENDDO
-       END DO
-    END DO
-    ZAVGPGP = ZERRSUM/REAL(NGPTOT,JPRB)
-
-    CALL ERROR_PRINT(NAME, ZMINVAL, ZMAXVAL, ZMAXERR, ZERRSUM, ZSUM, ZAVGPGP, NDIM=2)
-  END SUBROUTINE VALIDATE_R2
-
-  SUBROUTINE VALIDATE_R3(NAME, REF, FIELD, NLON, NLEV, NDIM, NGPTOT, NBLOCKS)
-    ! Computes and prints errors "in the L1 norm sense"
-    CHARACTER(*), INTENT(IN) :: NAME
-    REAL(KIND=JPRB), INTENT(INOUT) :: REF(:,:,:,:), FIELD(:,:,:,:)
-    INTEGER(KIND=JPIM), INTENT(IN) :: NLON, NLEV, NDIM, NBLOCKS, NGPTOT
-
-    INTEGER :: B, BSIZE, JL, JK, JM
-    REAL(KIND=JPRB) :: ZMINVAL, ZMAXVAL, ZDIFF, ZMAXERR, ZERRSUM, ZSUM, ZRELERR, ZAVGPGP
-
-    ZMINVAL = +HUGE(ZMINVAL)
-    ZMAXVAL = -HUGE(ZMAXVAL)
-    ZMAXERR = 0.0_JPRB
-    ZERRSUM = 0.0_JPRB
-    ZSUM = 0.0_JPRB
-
-    !OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(B, BSIZE) &
-    !& REDUCTION(MIN:ZMINVAL, MAX:ZMAXVAL, MAX:ZMAXERR, +:ZERRSUM, +:ZSUM)
-    DO B=1, NBLOCKS
-       BSIZE = MIN(NLON, NGPTOT - (B-1)*NLON)  ! Field block size
-       ZMINVAL = MIN(ZMINVAL,MINVAL(FIELD(:,:,:,B)))
-       ZMAXVAL = MAX(ZMAXVAL,MAXVAL(FIELD(:,:,:,B)))
-       DO JM=1, NDIM
-          DO JL=1, NLEV
-             DO JK=1, bsize
-                ! Difference against reference result in one-norm sense
-                ZDIFF = ABS(FIELD(JK,JL,JM,B) - REF(JK,JL,JM,B))
-                ZMAXERR = MAX(ZMAXERR,ZDIFF)
-                ZERRSUM = ZERRSUM + ZDIFF
-                ZSUM = ZSUM + ABS(REF(JK,JL,JM,B))
-             END DO
-          END DO
-       END DO
-    END DO
-    ZAVGPGP = ZERRSUM/REAL(NGPTOT,JPRB)
-
-    CALL ERROR_PRINT(NAME, ZMINVAL, ZMAXVAL, ZMAXERR, ZERRSUM, ZSUM, ZAVGPGP, NDIM=2)
-  END SUBROUTINE VALIDATE_R3
-
-  SUBROUTINE ERROR_PRINT(NAME, ZMINVAL, ZMAXVAL, ZMAXERR, ZERRSUM, ZSUM, ZAVGPGP, NDIM)
-    ! Print error statistic for a single variable (adapted from diff_mod.F90)
-    CHARACTER(*), INTENT(IN) :: NAME
-    REAL(KIND=JPRB), INTENT(IN) :: ZMINVAL, ZMAXVAL, ZMAXERR, ZERRSUM, ZSUM, ZAVGPGP
-    INTEGER(KIND=JPIM), INTENT(IN) :: NDIM
-    REAL(KIND=JPRB) :: zrelerr
-    REAL(KIND=JPRB), parameter :: zeps = epsilon(1.0_JPRB)
-    INTEGER :: IOPT
-    character(len=5) clwarn
-
-    iopt = 0
-    if (zerrsum < zeps) then
-       zrelerr = 0.0_JPRB
-       iopt = 1
-    elseif (zsum < zeps) then
-       zrelerr = zerrsum/(1.0_JPRB + zsum)
-       iopt = 2
-    else
-       zrelerr = zerrsum/zsum
-       iopt = 3
-    endif
-
-    !-- If you get 4 exclamation marks next to your error output,
-    !   then it is likely that some uninitialized variables exists or
-    !   some other screw-up -- watch out this !!!!
-    clwarn = ' '
-    if (zrelerr > 10.0_JPRB * zeps) clwarn = ' !!!!'
-    zrelerr = 100.0_JPRB * zrelerr
-
-    write(*,1000) name,ndim,iopt, &
-         & zminval,zmaxval, zmaxerr, zavgpgp, zrelerr, clwarn
-1000 format(1X,A20,1X,I1,'D',I1,5(1X,E20.13),A)
-
-  END SUBROUTINE ERROR_PRINT
 
 END MODULE CLOUDSC_GLOBAL_STATE_MOD
