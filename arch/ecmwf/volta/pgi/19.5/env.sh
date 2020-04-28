332c6456aeba56a6bbabe46e4d73712c8ee7be8f
Michael Lange <Michael.Lange@ecmwf.int>
2020-04-28 15:13:08 +0100

    Bundle: Add PGI host arch end env configuration for Volta

---
+++ arch/ecmwf/volta/pgi/19.5/env.sh
@@ -0,0 +1,35 @@
+# Source me to get the correct configure/build/run environment
+
+# Store tracing and disable (module is *way* too verbose)
+{ tracing_=${-//[^x]/}; set +x; } 2>/dev/null
+
+module_load() {
+  echo "+ module load $1"
+  module load $1
+}
+module_unload() {
+  echo "+ module unload $1"
+  module unload $1
+}
+
+# Unload to be certain
+module_unload boost
+module_unload cmake
+module_unload intel
+module_unload pgi
+module_unload gnu
+
+# Load modules
+module_load pgi/19.5
+module_load boost/1.61.0
+module_load cmake/3.14.5
+
+set -x
+
+# Increase stack size to maximum
+ulimit -S -s unlimited
+
+# Restore tracing to stored setting
+if [[ -n "$tracing_" ]]; then set -x; else set +x; fi
+
+export ECBUILD_TOOLCHAIN=$PWD/../toolchains/ecmwf-volta-pgi-host.cmake
