8136bc8b05caf521505511c368627f5d3bc3a4b6
Michael Lange <Michael.Lange@ecmwf.int>
2020-05-26 14:06:49 +0100

    Bundle: Fix up the leap42 env files and toolchains

--- arch/toolchains/ecmwf-leap42-intel.cmake
+++ arch/toolchains/ecmwf-leap42-intel.cmake
@@ -10,7 +10,6 @@ set( ECBUILD_FIND_MPI OFF )
 
 # General Flags (add to default)
 
-set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -O2 -g")
 set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -qopenmp-threadprivate compat")
 set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -assume byterecl")
 set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -convert big_endian")
@@ -22,4 +21,4 @@ set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -finline-functions")
 set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -finline-limit=500")
 set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Winline")
 
-set(ECBUILD_C_FLAGS "-O2 -g -std=c11")
+set(ECBUILD_C_FLAGS "-std=c11")
