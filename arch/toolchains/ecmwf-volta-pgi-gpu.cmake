5365489fd4af321b8dc1dd4f8c104069a0a25050
Michael Lange <Michael.Lange@ecmwf.int>
2020-06-09 11:34:42 +0100

    GPU: Don't force OpenACC/OpenMP flags onto ECBUILD_Fortran_FLAGS

--- arch/toolchains/ecmwf-volta-pgi-gpu.cmake
+++ arch/toolchains/ecmwf-volta-pgi-gpu.cmake
@@ -2,32 +2,30 @@
 # COMPILER
 ####################################################################
 
-include(CMakeForceCompiler)
-
 set( ECBUILD_FIND_MPI OFF )
 
 ####################################################################
 # OpenMP FLAGS
 ####################################################################
 
-set( OMP_C_FLAGS             "-mp -mp=bind,allcores,numa" )
-set( OMP_CXX_FLAGS           "-mp -mp=bind,allcores,numa" )
-set( OMP_Fortran_FLAGS       "-mp -mp=bind,allcores,numa" )
+set( OpenMP_C_FLAGS             "-mp -mp=bind,allcores,numa" )
+set( OpenMP_CXX_FLAGS           "-mp -mp=bind,allcores,numa" )
+set( OpenMP_Fortran_FLAGS       "-mp -mp=bind,allcores,numa" )
 
 ####################################################################
 # OpenAcc FLAGS
 ####################################################################
 
-set( OACC_Fortran_FLAGS "-acc -ta=tesla:lineinfo,deepcopy,maxregcount:100,fastmath" )
-set( OACC_Fortran_FLAGS "${OACC_Fortran_FLAGS} -Mvect=levels:6" )
-set( OACC_Fortran_FLAGS "${OACC_Fortran_FLAGS} -Mconcur=levels:6" )
-set( OACC_Fortran_FLAGS "${OACC_Fortran_FLAGS} -Minfo=accel" )
+set( OpenACC_Fortran_FLAGS "-acc -ta=tesla:lineinfo,deepcopy,maxregcount:100,fastmath" )
+set( OpenACC_Fortran_FLAGS "${OpenACC_Fortran_FLAGS} -Mvect=levels:6" )
+set( OpenACC_Fortran_FLAGS "${OpenACC_Fortran_FLAGS} -Mconcur=levels:6" )
+set( OpenACC_Fortran_FLAGS "${OpenACC_Fortran_FLAGS} -Minfo" )
 
 ####################################################################
 # COMMON FLAGS
 ####################################################################
 
-set(ECBUILD_Fortran_FLAGS "${OACC_Fortran_FLAGS} -fpic")
+set(ECBUILD_Fortran_FLAGS "-fpic")
 set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mframe")
 set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mbyteswapio")
 set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mstack_arrays")
@@ -38,17 +36,10 @@ set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mdaz")
 
 set( ECBUILD_Fortran_FLAGS_BIT "-O2 -gopt" )
 
-set( ECBUILD_C_FLAGS "-O2 -gopt ${OMP_C_FLAGS} -traceback" )
+set( ECBUILD_C_FLAGS "-O2 -gopt -traceback" )
 
-set( ECBUILD_CXX_FLAGS "-O2 -gopt ${OMP_CXX_FLAGS}" )
+set( ECBUILD_CXX_FLAGS "-O2 -gopt" )
 
 # Fix for C++ template headers needed for Serialbox
 set( GNU_HEADER_INCLUDE "-I/usr/local/apps/gcc/7.3.0/lib/gcc/x86_64-linux-gnu/7.3.0/include-fixed" )
 set( ECBUILD_CXX_FLAGS "${ECBUILD_CXX_FLAGS} ${GNU_HEADER_INCLUDE}" )
-
-####################################################################
-# LINK FLAGS
-####################################################################
-
-set( ECBUILD_SHARED_LINKER_FLAGS "-Wl,--as-needed" )
-set( ECBUILD_EXE_LINKER_FLAGS    "-Wl,--as-needed" )
