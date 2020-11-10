d51e4c368eba9412553ec046c295d743ca87143b
Michael Lange <Michael.Lange@ecmwf.int>
2020-11-10 08:15:27 +0000

    Loki: Add an intermediate placeholder header file for CI testing
    This will be required for CI regression tests to during the upcomnig
    switch in Fortran-C transpilation in the Loki source-to-source toolchain.

---
+++ src/common/include/parkind1_c.h
@@ -0,0 +1,8 @@
+/* 
+   Placeholder for an eventually auto-generated C header of parkind1.F90.
+
+   This is a trivial intermediate step for ensuring automated CI tests pass
+   during the migration to a more versatile Fortran-to-C transpilation toolchain 
+   using Loki source-to-source translation. This file should be removed as soon
+   as that migration is done. I solemnly swear... ;)
+*/
