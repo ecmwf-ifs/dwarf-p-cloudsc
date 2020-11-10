c9489e96800888dbb433b33632885f3f501158e3
Michael Lange <Michael.Lange@ecmwf.int>
2020-11-10 08:29:38 +0000

    Loki: Remove placeholder header file again

--- src/common/include/parkind1_c.h
+++
@@ -1,8 +0,0 @@
-/* 
-   Placeholder for an eventually auto-generated C header of parkind1.F90.
-
-   This is a trivial intermediate step for ensuring automated CI tests pass
-   during the migration to a more versatile Fortran-to-C transpilation toolchain 
-   using Loki source-to-source translation. This file should be removed as soon
-   as that migration is done. I solemnly swear... ;)
-*/
