64fdabffc7bebe7b97a6287a5722c6c76ec4f98c
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2020-12-21 16:37:03 +0000

    CLOUDSC_MPI_MOD: Add single precision versions of communication routines

--- src/common/module/cloudsc_mpi_mod.F90
+++ src/common/module/cloudsc_mpi_mod.F90
@@ -1,7 +1,7 @@
 module cloudsc_mpi_mod
   ! Helper module to handle MPI communication and environment
 
-  use parkind1, only : jpim, jprd
+  use parkind1, only : jpim, jprd, jprs
 
 #ifdef HAVE_MPI
   use cloudsc_mpif
@@ -13,35 +13,35 @@ module cloudsc_mpi_mod
   integer(kind=jpim) :: irank   = 0  ! local process number
 
   interface cloudsc_mpi_reduce
-    procedure cloudsc_mpi_reduce_jprd, cloudsc_mpi_reduce_int
+    procedure cloudsc_mpi_reduce_jprd, cloudsc_mpi_reduce_jprs, cloudsc_mpi_reduce_int
   end interface cloudsc_mpi_reduce
 
   interface cloudsc_mpi_reduce_sum
 #ifdef HAVE_MPI
-    procedure cloudsc_mpi_reduce_sum_jprd, cloudsc_mpi_reduce_sum_int
+    procedure cloudsc_mpi_reduce_sum_jprd, cloudsc_mpi_reduce_sum_jprs, cloudsc_mpi_reduce_sum_int
 #else
-    procedure cloudsc_mpi_reduce_dummy_jprd, cloudsc_mpi_reduce_dummy_int
+    procedure cloudsc_mpi_reduce_dummy_jprd, cloudsc_mpi_reduce_dummy_jprs, cloudsc_mpi_reduce_dummy_int
 #endif
   end interface cloudsc_mpi_reduce_sum
 
   interface cloudsc_mpi_reduce_min
 #ifdef HAVE_MPI
-    procedure cloudsc_mpi_reduce_min_jprd, cloudsc_mpi_reduce_min_int
+    procedure cloudsc_mpi_reduce_min_jprd, cloudsc_mpi_reduce_min_jprs, cloudsc_mpi_reduce_min_int
 #else
-    procedure cloudsc_mpi_reduce_dummy_jprd, cloudsc_mpi_reduce_dummy_int
+    procedure cloudsc_mpi_reduce_dummy_jprd, cloudsc_mpi_reduce_dummy_jprs, cloudsc_mpi_reduce_dummy_int
 #endif
   end interface cloudsc_mpi_reduce_min
 
   interface cloudsc_mpi_reduce_max
 #ifdef HAVE_MPI
-    procedure cloudsc_mpi_reduce_max_jprd, cloudsc_mpi_reduce_max_int
+    procedure cloudsc_mpi_reduce_max_jprd, cloudsc_mpi_reduce_max_jprs, cloudsc_mpi_reduce_max_int
 #else
-    procedure cloudsc_mpi_reduce_dummy_jprd, cloudsc_mpi_reduce_dummy_int
+    procedure cloudsc_mpi_reduce_dummy_jprd, cloudsc_mpi_reduce_dummy_jprs, cloudsc_mpi_reduce_dummy_int
 #endif
   end interface cloudsc_mpi_reduce_max
 
   interface cloudsc_mpi_gather
-    procedure cloudsc_mpi_gather_jprd, cloudsc_mpi_gather_int
+    procedure cloudsc_mpi_gather_jprd, cloudsc_mpi_gather_jprs, cloudsc_mpi_gather_int
   end interface cloudsc_mpi_gather
 
 contains
@@ -116,6 +116,32 @@ contains
 #endif
   end subroutine cloudsc_mpi_reduce_jprd
 
+  subroutine cloudsc_mpi_reduce_jprs(buf, nelem, op, iroot)
+    ! cloudsc_mpi_reduce_jprs - implementation of cloudsc_mpi_reduce for single
+
+    real(kind=jprs)   , intent(inout) :: buf(*)
+    integer(kind=jpim), intent(in)    :: nelem
+    integer           , intent(in)    :: op
+    integer(kind=jpim), intent(in)    :: iroot
+#ifdef HAVE_MPI
+    integer(kind=jpim)                :: ierror
+    logical 			      :: is_initialized
+
+    ! bail out early in case mpi was never initialized
+    call mpi_initialized(is_initialized, ierror)
+    if (.not. is_initialized) return
+
+    if (irank == iroot) then
+      call mpi_reduce(MPI_IN_PLACE, buf, nelem, &
+        & MPI_FLOAT, op, iroot, MPI_COMM_WORLD, ierror)
+    else
+      call mpi_reduce(buf, 0, nelem, &
+        & MPI_FLOAT, op, iroot, MPI_COMM_WORLD, ierror)
+    end if
+    if (ierror /= 0) call abor1('cloudsc_mpi: mpi_reduce failed')
+#endif
+  end subroutine cloudsc_mpi_reduce_jprs
+
   subroutine cloudsc_mpi_reduce_int(buf, nelem, op, iroot)
     ! cloudsc_mpi_reduce_int - implementation of cloudsc_mpi_reduce for int
 
@@ -151,6 +177,13 @@ contains
     call cloudsc_mpi_reduce(buf, nelem, MPI_SUM, iroot)
   end subroutine cloudsc_mpi_reduce_sum_jprd
 
+  subroutine cloudsc_mpi_reduce_sum_jprs(buf, nelem, iroot)
+    real(kind=jprs), intent(inout) :: buf(*)
+    integer(kind=jpim), intent(in) :: nelem, iroot
+
+    call cloudsc_mpi_reduce(buf, nelem, MPI_SUM, iroot)
+  end subroutine cloudsc_mpi_reduce_sum_jprs
+
   subroutine cloudsc_mpi_reduce_sum_int(buf, nelem, iroot)
     integer           , intent(inout) :: buf(*)
     integer(kind=jpim), intent(in) :: nelem, iroot
@@ -164,6 +197,13 @@ contains
 
     call cloudsc_mpi_reduce(buf, nelem, MPI_MIN, iroot)
   end subroutine cloudsc_mpi_reduce_min_jprd
+
+  subroutine cloudsc_mpi_reduce_min_jprs(buf, nelem, iroot)
+    real(kind=jprs), intent(inout) :: buf(*)
+    integer(kind=jpim), intent(in) :: nelem, iroot
+
+    call cloudsc_mpi_reduce(buf, nelem, MPI_MIN, iroot)
+  end subroutine cloudsc_mpi_reduce_min_jprs
 
   subroutine cloudsc_mpi_reduce_min_int(buf, nelem, iroot)
     integer           , intent(inout) :: buf(*)
@@ -179,6 +219,13 @@ contains
     call cloudsc_mpi_reduce(buf, nelem, MPI_MAX, iroot)
   end subroutine cloudsc_mpi_reduce_max_jprd
 
+  subroutine cloudsc_mpi_reduce_max_jprs(buf, nelem, iroot)
+    real(kind=jprs), intent(inout) :: buf(*)
+    integer(kind=jpim), intent(in) :: nelem, iroot
+
+    call cloudsc_mpi_reduce(buf, nelem, MPI_MAX, iroot)
+  end subroutine cloudsc_mpi_reduce_max_jprs
+
   subroutine cloudsc_mpi_reduce_max_int(buf, nelem, iroot)
     integer           , intent(inout) :: buf(*)
     integer(kind=jpim), intent(in) :: nelem, iroot
@@ -192,6 +239,11 @@ contains
     real(kind=jprd),    intent(inout) :: buf(*)
     integer(kind=jpim), intent(in)    :: nelem, iroot
   end subroutine cloudsc_mpi_reduce_dummy_jprd
+
+  subroutine cloudsc_mpi_reduce_dummy_jprs(buf, nelem, iroot)
+    real(kind=jprs),    intent(inout) :: buf(*)
+    integer(kind=jpim), intent(in)    :: nelem, iroot
+  end subroutine cloudsc_mpi_reduce_dummy_jprs
 
   subroutine cloudsc_mpi_reduce_dummy_int(buf, nelem, iroot)
     integer,            intent(inout) :: buf(*)
@@ -219,6 +271,26 @@ contains
     recvbuf(:,:,1) = sendbuf(:,:)
 #endif
   end subroutine cloudsc_mpi_gather_jprd
+
+  subroutine cloudsc_mpi_gather_jprs(sendbuf, sendcount, recvbuf, recvcount, iroot)
+    real(kind=jprs),    intent(in)  :: sendbuf(:,:)
+    real(kind=jprs),    intent(out) :: recvbuf(:,:,:)
+    integer(kind=jpim), intent(in)  :: sendcount, recvcount, iroot
+
+#ifdef HAVE_MPI
+    integer(kind=jpim) :: ierror
+    logical :: is_initialized
+
+    ! bail out early in case mpi was never initialized
+    call mpi_initialized(is_initialized, ierror)
+    if (.not. is_initialized) return
+
+    call mpi_gather(sendbuf, sendcount, MPI_FLOAT, recvbuf, recvcount, MPI_FLOAT, iroot, MPI_COMM_WORLD, ierror)
+    if (ierror /= 0) call abor1('cloudsc_mpi: mpi_gather failed')
+#else
+    recvbuf(:,:,1) = sendbuf(:,:)
+#endif
+  end subroutine cloudsc_mpi_gather_jprs
 
   subroutine cloudsc_mpi_gather_int(sendbuf, sendcount, recvbuf, recvcount, iroot)
     integer,            intent(in)  :: sendbuf(:,:)
