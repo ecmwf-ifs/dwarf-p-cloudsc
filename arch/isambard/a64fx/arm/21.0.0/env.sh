92f32acaee386d11d6f9ba31de63b75634b0d9f4
Balthasar Reuter <Balthasar.Reuter@ecmwf.int>
2021-04-14 13:29:17 +0000

    Arch files for ARM@Isambard

---
+++ arch/isambard/a64fx/arm/21.0.0/env.sh
@@ -0,0 +1,19 @@
+
+[ -d $HOME/arm-sve-tools ] || cp -a ~brx-pridley/arm-sve-tools $HOME
+source $HOME/arm-sve-tools/isambard-arm.bashrc
+
+# Requires manual HDF5 installation, e.g. via the script provided
+# in https://git.ecmwf.int/users/nabr/repos/build-scripts 
+source $HOME/deps/arm/21.0.0/deps_env.sh
+export HDF5_DIR=$HOME/deps/arm/21.0.0
+
+export C_INCLUDES=-I$HDF5_DIR/include
+export FC_INCLUDES=-I$HDF5_DIR/include
+
+export CC=armclang
+export CXX=armclang++
+export FC=armflang
+
+module load cmake
+
+export ECBUILD_TOOLCHAIN="./toolchain.cmake"
