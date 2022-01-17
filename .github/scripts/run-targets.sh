a4a9984b4077dac86bf88babc1b58c1b86f0b835
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2022-01-17 09:07:51 +0000

    Github: verify targets by executing with default settings

---
+++ .github/scripts/run-targets.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+set -x
+
+exit_code=0
+
+#
+# Run each of the binaries using default arguments and validate exit codes
+#
+
+for target in $(ls build/bin)
+do
+  if [[ "$mpi_flag" == "--with-mpi" ]]
+  then
+    # Two ranks with one thread each
+    mpirun -np 2 bin/$target 1
+  else
+    # Two threads
+    bin/$target 2
+  fi
+  exit_code=$((exit_code + $?))
+done
+
+exit $exit_code
