f878d2d7ee29bbc0f798334bbc0aa56dfe805843
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2022-07-08 11:51:32 +0000

    Adress review comments

--- arch/toolchains/ecmwf-volta-pgi-gpu.cmake
+++ arch/toolchains/ecmwf-volta-pgi-gpu.cmake
@@ -16,11 +16,16 @@ set( ECBUILD_FIND_MPI ON )
 # OpenMP FLAGS
 ####################################################################
 
-# Note: OpenMP_<lang>_FLAGS gets overwritten by the FindOpenMP module
+# Note: OpenMP_Fortran_FLAGS gets overwritten by the FindOpenMP module
 # unless its stored as a cache variable
-set( OpenMP_C_FLAGS             "-mp -mp=bind,allcores,numa" CACHE STRING "" FORCE )
-set( OpenMP_CXX_FLAGS           "-mp -mp=bind,allcores,numa" CACHE STRING "" FORCE )
-set( OpenMP_Fortran_FLAGS       "-mp -mp=gpu,bind,allcores,numa" CACHE STRING "" FORCE )
+set( OpenMP_Fortran_FLAGS   "-mp -mp=gpu,bind,allcores,numa" CACHE STRING "" )
+
+# Note: OpenMP_C_FLAGS and OpenMP_C_LIB_NAMES have to be provided _both_ to
+# keep FindOpenMP from overwriting the FLAGS variable (the cache entry alone
+# doesn't have any effect here as the module uses FORCE to overwrite the
+# existing value)
+set( OpenMP_C_FLAGS         "-mp -mp=bind,allcores,numa" CACHE STRING "" )
+set( OpenMP_C_LIB_NAMES     "acchost" CACHE STRING "")
 
 ####################################################################
 # OpenAcc FLAGS
@@ -28,7 +33,7 @@ set( OpenMP_Fortran_FLAGS       "-mp -mp=gpu,bind,allcores,numa" CACHE STRING ""
 
 # NB: We have to add `-mp` again to avoid undefined symbols during linking
 # (smells like an Nvidia bug)
-set( OpenACC_Fortran_FLAGS "-acc=gpu -mp=gpu -gpu=cc70,fastmath,lineinfo" )
+set( OpenACC_Fortran_FLAGS "-acc=gpu -mp=gpu -gpu=cc80,lineinfo,fastmath" CACHE STRING "" )
 # Enable this to get more detailed compiler output
 # set( OpenACC_Fortran_FLAGS "${OpenACC_Fortran_FLAGS} -Minfo" )
 
