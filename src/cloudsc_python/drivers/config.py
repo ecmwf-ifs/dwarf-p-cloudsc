671742cd845c7608aca293e98e33a65ec1aa3fe5
Stefano Ubbiali <subbiali@phys.ethz.ch>
2022-09-28 07:49:04 +0200

    Add IOConfig and FortranConfig.

--- src/cloudsc_python/drivers/config.py
+++ src/cloudsc_python/drivers/config.py
@@ -1,14 +1,50 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 import numpy as np
-from os.path import dirname, join, normpath
+from os.path import dirname, join, normpath, splitext
 from pydantic import BaseModel, validator
+import socket
 from typing import Optional
 
 from cloudsc4py.framework.config import DataTypes, GT4PyConfig
 
 
-class Config(BaseModel):
+class IOConfig(BaseModel):
+    output_file: Optional[str]
+    host_name: Optional[str]
+
+    @validator("output_file")
+    def check_extension(cls, v: Optional[str]) -> Optional[str]:
+        if v is None:
+            return v
+
+        basename, extension = splitext(v)
+        if extension == "":
+            return v + ".csv"
+        elif extension == ".csv":
+            return v
+        else:
+            return basename + ".csv"
+
+    @validator("host_name")
+    def set_host_name(cls, v: Optional[str]) -> str:
+        return v or socket.gethostname()
+
+    def with_host_name(self, host_name: str) -> IOConfig:
+        args = self.dict()
+        args["host_name"] = host_name
+        return IOConfig(**args)
+
+    def with_output_file(self, output_file: str) -> IOConfig:
+        args = self.dict()
+        args["output_file"] = output_file
+        return IOConfig(**args)
+
+
+default_io_config = IOConfig(output_file=None, host_name=None)
+
+
+class PythonConfig(BaseModel):
     # domain
     nx: Optional[int]
 
@@ -29,39 +65,39 @@ class Config(BaseModel):
     def add_dtypes(cls, v, values) -> GT4PyConfig:
         return v.with_dtypes(values["data_types"])
 
-    def with_backend(self, backend: Optional[str]) -> Config:
+    def with_backend(self, backend: Optional[str]) -> PythonConfig:
         args = self.dict()
         args["gt4py_config"] = GT4PyConfig(**args["gt4py_config"]).with_backend(backend).dict()
-        return Config(**args)
+        return PythonConfig(**args)
 
-    def with_checks(self, enabled: bool) -> Config:
+    def with_checks(self, enabled: bool) -> PythonConfig:
         args = self.dict()
         args["gt4py_config"] = (
             GT4PyConfig(**args["gt4py_config"]).with_validate_args(enabled).dict()
         )
         args["sympl_enable_checks"] = enabled
-        return Config(**args)
+        return PythonConfig(**args)
 
-    def with_num_runs(self, num_runs: Optional[int]) -> Config:
+    def with_num_runs(self, num_runs: Optional[int]) -> PythonConfig:
         args = self.dict()
         if num_runs is not None:
             args["num_runs"] = num_runs
-        return Config(**args)
+        return PythonConfig(**args)
 
-    def with_nx(self, nx: Optional[int]) -> Config:
+    def with_nx(self, nx: Optional[int]) -> PythonConfig:
         args = self.dict()
         if nx is not None:
             args["nx"] = nx
-        return Config(**args)
+        return PythonConfig(**args)
 
-    def with_validation(self, enabled: bool) -> Config:
+    def with_validation(self, enabled: bool) -> PythonConfig:
         args = self.dict()
         args["enable_validation"] = enabled
-        return Config(**args)
+        return PythonConfig(**args)
 
 
 config_files_dir = normpath(join(dirname(__file__), "../../../config-files"))
-default_config = Config(
+default_python_config = PythonConfig(
     nx=None,
     enable_validation=True,
     input_file=join(config_files_dir, "input.h5"),
@@ -71,3 +107,33 @@ default_config = Config(
     gt4py_config=GT4PyConfig(backend="numpy", rebuild=False, validate_args=True, verbose=True),
     sympl_enable_checks=True,
 )
+
+
+class FortranConfig(BaseModel):
+    mode: str
+    num_runs: int
+    num_threads: int
+    nx: int
+
+    def with_mode(self, mode: str) -> FortranConfig:
+        args = self.dict()
+        args["mode"] = mode
+        return FortranConfig(**args)
+
+    def with_num_runs(self, num_runs: int) -> FortranConfig:
+        args = self.dict()
+        args["num_runs"] = num_runs
+        return FortranConfig(**args)
+
+    def with_num_threads(self, num_threads: int) -> FortranConfig:
+        args = self.dict()
+        args["num_threads"] = num_threads
+        return FortranConfig(**args)
+
+    def with_nx(self, nx: int) -> FortranConfig:
+        args = self.dict()
+        args["nx"] = nx
+        return FortranConfig(**args)
+
+
+default_fortran_config = FortranConfig(mode="fortran", num_runs=1, num_threads=1, nx=1)
