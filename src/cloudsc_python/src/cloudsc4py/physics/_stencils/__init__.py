097009bd92c7c12b15c5a1e6bf8ca31dccd78bf6
Stefano Ubbiali <subbiali@phys.ethz.ch>
2022-11-03 14:38:59 +0100

    Add split version of CLOUDSC amenable to the FVM.

--- src/cloudsc_python/src/cloudsc4py/physics/_stencils/__init__.py
+++ src/cloudsc_python/src/cloudsc4py/physics/_stencils/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import cloudsc4py.physics._stencils.cloudsc
+import cloudsc4py.physics._stencils.cloudsc_split
 import cloudsc4py.physics._stencils.cuadjtq
 import cloudsc4py.physics._stencils.fccld
 import cloudsc4py.physics._stencils.fcttre
