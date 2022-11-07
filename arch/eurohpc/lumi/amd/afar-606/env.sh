6655185baac187aca76ce663bdabec0a23cec0fc
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2022-11-07 18:52:40 +0200

    LUMI arch files (WIP)

---
+++ arch/eurohpc/lumi/amd/afar-606/env.sh
@@ -0,0 +1,48 @@
+# (C) Copyright 1988- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+# Source me to get the correct configure/build/run environment
+
+# Store tracing and disable (module is *way* too verbose)
+{ tracing_=${-//[^x]/}; set +x; } 2>/dev/null
+
+module_load() {
+  echo "+ module load $1"
+  module load $1
+}
+module_unload() {
+  echo "+ module unload $1"
+  module unload $1
+}
+
+# Unload to be certain
+module reset
+
+module use /pfs/lustrep1/projappl/project_465000116/bareuter/rocm/mymodules
+
+# Load modules
+module_load LUMI/22.06
+module_load PrgEnv-aocc/8.3.3
+module_load partition/G
+module_load rocm/afar-606
+module_load buildtools/22.06
+module_load cray-python/3.9.12.1
+
+# Load hand-installed dependencies
+export HDF5_DIR=/users/bareuter/hdf5/1.12.1/rocm/afar-432
+export HDF5_ROOT=$HDF5_DIR
+
+# Specify compilers
+export CC=amdclang CXX=amdclang++ FC=amdflang
+
+set -x
+
+# Restore tracing to stored setting
+{ if [[ -n "$tracing_" ]]; then set -x; else set +x; fi } 2>/dev/null
+
+export ECBUILD_TOOLCHAIN="./toolchain.cmake"
---
+++ arch/eurohpc/lumi/amd/afar-606/toolchain.cmake
@@ -0,0 +1,42 @@
+# (C) Copyright 1988- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+####################################################################
+# COMPILER
+####################################################################
+
+set( ECBUILD_FIND_MPI ON )
+
+####################################################################
+# OpenMP FLAGS
+####################################################################
+
+set( OpenMP_C_FLAGS "-fopenmp=libomp" CACHE STRING "" )
+set( OpenMP_C_LIB_NAMES "omp;pthread" CACHE STRING "" )
+set( OpenMP_Fortran_FLAGS   "-fopenmp -fopenmp-targets=amdgcn-amd-amdhsa -Xopenmp-target=amdgcn-amd-amdhsa -march=gfx90a" CACHE STRING "" )
+set( OpenMP_Fortran_LIB_NAMES "omp;omptarget;pthread" CACHE STRING "" )
+set( OpenMP_omptarget_LIBRARY "/pfs/lustrep1/projappl/project_465000116/bareuter/rocm/afar/606/llvm/lib/libomptarget.so" CACHE FILEPATH "" )
+
+####################################################################
+# OpenAcc FLAGS
+####################################################################
+
+set( ENABLE_ACC OFF CACHE STRING "" )
+
+####################################################################
+# COMMON FLAGS
+####################################################################
+
+set(ECBUILD_Fortran_FLAGS "-v -fpic")
+#set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mframe")
+#set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mbyteswapio")
+#set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mstack_arrays")
+#set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mrecursive")
+#set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Ktrap=fp")
+#set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Kieee")
+#set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -Mdaz")
