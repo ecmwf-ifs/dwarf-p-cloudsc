6c4c2a217d29f564e18a4992dc07d9b20b98f177
Zbigniew Piotrowski <zbigniew.piotrowski@ecmwf.int>
2023-02-07 18:47:00 +0100

    First round of cmake cleanup

--- src/cloudsc_pyiface/fortransrc/README.md
+++
@@ -1,3 +0,0 @@
-1 git cp cloudsc-fortran cloudsc-pyiface
-2 cp cloudsc-fortran2 cloudsc-pyiface
-3 switch off default build of cloudsc-pyiface Fortran driver
--- src/cloudsc_pyiface/run_python_driven_cloudsc.sh
+++
@@ -1,8 +0,0 @@
-module load python3
-python3 -m venv ~/cstest
-source ~/cstest/bin/activate
-pip install --upgrade pip
-pip install f90wrap h5py 
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:.
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
-ipython ./dwarfdriver.py
