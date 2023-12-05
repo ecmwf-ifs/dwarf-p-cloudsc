cef2017ef28671fd8266143c9e6bebbe4ba26895
Michael Staneker <michael.staneker@ecmwf.int>
2023-12-05 12:30:39 +0000

    Enabling usage of HDF5 for C-style variants (in addition to serialbox), e.g., cloudsc_c

--- src/cloudsc_c/cloudsc/cloudsc_validate.c
+++ src/cloudsc_c/cloudsc/cloudsc_validate.c
@@ -154,7 +154,7 @@ void validate_3d(const char *name, double *v_ref, double *v_field, int nlon,
 }
 
 
-int cloudsc_validate(const int nlon, const int nlev, const int nclv, const int ngptot, const int nproma,
+void cloudsc_validate(const int nlon, const int nlev, const int nclv, const int ngptot, const int nproma,
 		     double *plude, double *pcovptot, double *prainfrac_toprfz, double *pfsqlf, double *pfsqif,
 		     double *pfcqlng, double *pfcqnng, double *pfsqrf, double *pfsqsf, double *pfcqrng, double *pfcqsng,
 		     double *pfsqltur, double *pfsqitur, double *pfplsl, double *pfplsn, double *pfhpsl, double *pfhpsn,
