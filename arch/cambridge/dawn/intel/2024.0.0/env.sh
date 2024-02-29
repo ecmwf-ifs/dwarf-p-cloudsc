283e0c7856f69d1655e8bcf891491372afad89f6
Michael Staneker <michael.staneker@ecmwf.int>
2024-02-29 14:44:34 +0000

    arch/env file for 'dawn' in cambridge (to compile SYCL variants)

---
+++ arch/cambridge/dawn/intel/2024.0.0/env.sh
@@ -0,0 +1,37 @@
+# (C) Copyright 1988- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+# Source me to get the correct configure/build/run environment
+
+# Store tracing and disable (module is *way* too verbose)
+{ tracing_=${-//[^x]/}; set +x; } 2>/dev/null
+
+module_load() {
+  echo "+ module load $1"
+  module load $1
+}
+module_unload() {
+  echo "+ module unload $1"
+  module unload $1
+}
+
+# Unload all modules to be certain
+module purge
+
+# Load modules
+module_load default-dawn
+module_load intel-oneapi-compilers/2024.0.0
+module_load boost/1.83.0
+module_load cmake/3.27.9
+
+set -x
+
+# Restore tracing to stored setting
+{ if [[ -n "$tracing_" ]]; then set -x; else set +x; fi } 2>/dev/null
+
+export ECBUILD_TOOLCHAIN="./toolchain.cmake"
