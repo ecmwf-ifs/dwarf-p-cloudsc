e75fcb7f2740144983b7d633e305b5cca9bcd996
Slavko Brdar <slavko.brdar@ecmwf.int>
2024-04-16 11:16:23 +0000

    adapt to FIELD_API implementation

--- src/cloudsc_gpu_atlas/cloudsc_driver_mod.F90
+++ src/cloudsc_gpu_atlas/cloudsc_driver_mod.F90
@@ -8,13 +8,12 @@
 ! nor does it submit to any jurisdiction.
 
 MODULE CLOUDSC_DRIVER_MOD
-  USE PARKIND1, ONLY: JPIM, JPIB, JPRB, JPRD
+  USE PARKIND1, ONLY: JPIM, JPRB
   USE YOMPHYDER, ONLY: STATE_TYPE
-  USE YOECLDP, ONLY : NCLV
+  USE YOECLDP, ONLY : NCLV, YRECLDP
   USE CLOUDSC_MPI_MOD, ONLY: NUMPROC, IRANK
   USE TIMER_MOD, ONLY : PERFORMANCE_TIMER, GET_THREAD_NUM
-  USE EC_PMON_MOD, ONLY: EC_PMON
-  USE CLOUDSC_GLOBAL_ATLAS_STATE_MOD, ONLY: CLOUDSC_GLOBAL_ATLAS_STATE_BLOCK_VIEW
+  USE CLOUDSC_GPU_SCC_MOD, ONLY: CLOUDSC_SCC
 
   USE ATLAS_MODULE
   USE, INTRINSIC :: ISO_C_BINDING
@@ -23,8 +22,6 @@ MODULE CLOUDSC_DRIVER_MOD
   IMPLICIT NONE
 
 CONTAINS
-
-#INCLUDE "cloudsc.F90"
 
   SUBROUTINE CLOUDSC_DRIVER(FSET, NUMOMP, NGPTOTG, KFLDX, PTSPHY)
   
@@ -35,7 +32,7 @@ CONTAINS
     INTEGER(KIND=JPIM), INTENT(IN)    :: NUMOMP, NGPTOTG, KFLDX
     REAL(KIND=JPRB), INTENT(IN)   :: PTSPHY       ! Physics timestep
 
-    TYPE(CLOUDSC_GLOBAL_ATLAS_STATE_BLOCK_VIEW) :: FBLOCK
+    !TYPE(CLOUDSC_GLOBAL_ATLAS_STATE_BLOCK_VIEW) :: FBLOCK
     TYPE(ATLAS_FUNCTIONSPACE_BLOCKSTRUCTUREDCOLUMNS) :: FSPACE
     TYPE(ATLAS_FIELD) :: FIELD
     INTEGER(KIND=JPIM)    :: NPROMA, NLEV, NGPTOT
@@ -44,13 +41,14 @@ CONTAINS
 
     TYPE(PERFORMANCE_TIMER) :: TIMER
     INTEGER(KIND=JPIM) :: TID ! thread id from 0 .. NUMOMP - 1
-    INTEGER(KIND=JPIB) :: ENERGY, POWER, POWER_TOTAL, POWER_MAX, POWER_COUNT
-    LOGICAL            :: LEC_PMON = .FALSE.
-    CHARACTER(LEN=1)   :: CLEC_PMON
     TYPE(ATLAS_TRACE)  :: TRACE
 
-    !$acc routine(CLOUDSC)
-
+    ! input variables
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PLCRIT_AER(:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PICRIT_AER(:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PRE_ICE(:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PCCN(:,:,:)     ! liquid cloud condensation nuclei
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PNICE(:,:,:)    ! ice number concentration (cf. CCN)
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PT(:,:,:)       ! T at start of callpar
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PQ(:,:,:)       ! Q at start of callpar
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PVFA(:,:,:)     ! CC from VDF scheme
@@ -63,26 +61,29 @@ CONTAINS
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PHRLW(:,:,:)    ! Long-wave heating rate
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PVERVEL(:,:,:)  ! Vertical velocity
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PAP(:,:,:)      ! Pressure on full levels
-    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PAPH(:,:,:)     ! Pressure on half levels
-    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PLSM(:,:)       ! Land fraction (0-1)
-    LOGICAL, POINTER, CONTIGUOUS          :: LDCUM(:,:)      ! Convection active
-    INTEGER(KIND=JPIM),POINTER, CONTIGUOUS:: KTYPE(:,:)      ! Convection type 0,1,2
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PLU(:,:,:)      ! Conv. condensate
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PLUDE(:,:,:)    ! Conv. detrained water
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PSNDE(:,:,:)    ! Conv. detrained snow
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PMFU(:,:,:)     ! Conv. mass flux up
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PMFD(:,:,:)     ! Conv. mass flux down
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PA(:,:,:)       ! Original Cloud fraction (t)
-    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PCLV(:,:,:,:)
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PSUPSAT(:,:,:)
-    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PLCRIT_AER(:,:,:)
-    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PICRIT_AER(:,:,:)
-    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PRE_ICE(:,:,:)
-    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PCCN(:,:,:)     ! liquid cloud condensation nuclei
-    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PNICE(:,:,:)    ! ice number concentration (cf. CCN)
-    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PCOVPTOT(:,:,:)    ! Precip fraction
-    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PRAINFRAC_TOPRFZ(:,:)
-    ! Flux diagnostics for DDH budget
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: TENDENCY_CML_T(:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: TENDENCY_CML_A(:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: TENDENCY_CML_Q(:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: TENDENCY_TMP_T(:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: TENDENCY_TMP_A(:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: TENDENCY_TMP_Q(:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PLSM(:,:)       ! Land fraction (0-1)
+    LOGICAL, POINTER, CONTIGUOUS          :: LDCUM(:,:)      ! Convection active
+    INTEGER(KIND=JPIM),POINTER, CONTIGUOUS:: KTYPE(:,:)      ! Convection type 0,1,2
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PAPH(:,:,:)     ! Pressure on half levels
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PCLV(:,:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: TENDENCY_CML_CLD(:,:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: TENDENCY_TMP_CLD(:,:,:,:)
+
+    ! output variables
+    ! Note: flux diagnostics for DDH budget are named P*
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PFSQLF(:,:,:)    ! Flux of liquid
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PFSQIF(:,:,:)    ! Flux of ice
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PFCQLNG(:,:,:)   ! -ve corr for liq
@@ -97,21 +98,18 @@ CONTAINS
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PFPLSN(:,:,:)    ! ice+snow sedim flux
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PFHPSL(:,:,:)    ! Enthalpy flux for liq
     REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PFHPSN(:,:,:)    ! ice number concentration (cf. CCN)
-
-    TYPE(STATE_TYPE), POINTER, DIMENSION(:) :: TEND_CML
-    TYPE(STATE_TYPE), POINTER, DIMENSION(:) :: TEND_LOC
-    TYPE(STATE_TYPE), POINTER, DIMENSION(:) :: TEND_TMP
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PCOVPTOT(:,:,:)    ! Precip fraction
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: TENDENCY_LOC_T(:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: TENDENCY_LOC_A(:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: TENDENCY_LOC_Q(:,:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: PRAINFRAC_TOPRFZ(:,:)
+    REAL(KIND=JPRB), POINTER, CONTIGUOUS  :: TENDENCY_LOC_CLD(:,:,:,:)
 
     REAL(KIND=JPRB), POINTER :: TMP3D(:,:,:,:)
 
+print *, "exit 111"
+
     TRACE = ATLAS_TRACE("cloudsc_driver_mod.F90", __LINE__, "CLOUDSC_DRIVER","COMPUTE")
-
-    CALL GET_ENVIRONMENT_VARIABLE('EC_PMON', CLEC_PMON)
-    IF (CLEC_PMON == '1') LEC_PMON = .TRUE.
-
-    POWER_MAX = 0_JPIB
-    POWER_TOTAL = 0_JPIB
-    POWER_COUNT = 0_JPIB
 
     FIELD = FSET%FIELD("PCLV")
     FSPACE = FIELD%FUNCTIONSPACE()
@@ -128,6 +126,24 @@ CONTAINS
     ! Global timer for the parallel region
     CALL TIMER%START(NUMOMP)
 
+    !field = FSPACE%CREATE_FIELD(NAME="PLSM",   KIND=ATLAS_REAL(JPRB))
+
+!call field%allocate_device()
+!call field%update_device()
+
+print *, "exit"
+return
+
+    DO JKGLO = 1, 1 !FSET%SIZE()-2
+        print *, " device update for ", FIELD%NAME()
+        FIELD = FSET%FIELD(JKGLO)
+!        CALL FIELD%ALLOCATE_DEVICE()
+!        CALL FIELD%UPDATE_DEVICE()
+    END DO
+
+    return
+
+    ! input
     CALL FSET%DATA(1, PLCRIT_AER)
     CALL FSET%DATA(2, PICRIT_AER)
     CALL FSET%DATA(3, PRE_ICE)
@@ -152,151 +168,109 @@ CONTAINS
     CALL FSET%DATA(22, PMFD)
     CALL FSET%DATA(23, PA)
     CALL FSET%DATA(24, PSUPSAT)
-    CALL FSET%DATA(25, PLSM)
-    CALL FSET%DATA(26, LDCUM)
-    CALL FSET%DATA(27, KTYPE)
-    CALL FSET%DATA(28, PAPH)
-    CALL FSET%DATA(29, PCLV)
+    CALL FSET%DATA(25, TENDENCY_CML_T)
+    CALL FSET%DATA(26, TENDENCY_CML_A)
+    CALL FSET%DATA(27, TENDENCY_CML_Q)
+    CALL FSET%DATA(28, TENDENCY_TMP_T)
+    CALL FSET%DATA(29, TENDENCY_TMP_A)
+    CALL FSET%DATA(30, TENDENCY_TMP_Q)
+    CALL FSET%DATA(31, PLSM)
+    CALL FSET%DATA(32, LDCUM)
+    CALL FSET%DATA(33, KTYPE)
+    CALL FSET%DATA(34, PAPH)
+    CALL FSET%DATA(35, PCLV)
+    CALL FSET%DATA(36, TENDENCY_CML_CLD)
+    CALL FSET%DATA(37, TENDENCY_TMP_CLD)
 
-    CALL FSET%DATA(30, TMP3D)
+    ! output
+    CALL FSET%DATA(38, PFSQLF)
+    CALL FSET%DATA(39, PFSQIF)
+    CALL FSET%DATA(40, PFCQLNG)
+    CALL FSET%DATA(41, PFCQNNG)
+    CALL FSET%DATA(42, PFSQRF)
+    CALL FSET%DATA(43, PFSQSF)
+    CALL FSET%DATA(44, PFCQRNG)
+    CALL FSET%DATA(45, PFCQSNG)
+    CALL FSET%DATA(46, PFSQLTUR)
+    CALL FSET%DATA(47, PFSQITUR)
+    CALL FSET%DATA(48, PFPLSL)
+    CALL FSET%DATA(49, PFPLSN)
+    CALL FSET%DATA(50, PFHPSL)
+    CALL FSET%DATA(51, PFHPSN)
+    CALL FSET%DATA(52, PCOVPTOT)
+    CALL FSET%DATA(53, TENDENCY_LOC_T)
+    CALL FSET%DATA(54, TENDENCY_LOC_A)
+    CALL FSET%DATA(55, TENDENCY_LOC_Q)
+    CALL FSET%DATA(56, PRAINFRAC_TOPRFZ)
+    CALL FSET%DATA(57, TENDENCY_LOC_CLD)
 
-    DO JKGLO = 1, FSPACE%NBLKS()
-        TEND_CML(JKGLO)%T   => TMP3D(:,:,1,JKGLO)
-        TEND_CML(JKGLO)%A   => TMP3D(:,:,2,JKGLO)
-        TEND_CML(JKGLO)%Q   => TMP3D(:,:,3,JKGLO)
-        TEND_CML(JKGLO)%CLD => TMP3D(:,:,4:,JKGLO)
-    END DO
-    CALL FSET%DATA(31, TMP3D)
-    DO JKGLO = 1, FSPACE%NBLKS()
-        TEND_TMP(JKGLO)%T   => TMP3D(:,:,1,JKGLO)
-        TEND_TMP(JKGLO)%A   => TMP3D(:,:,2,JKGLO)
-        TEND_TMP(JKGLO)%Q   => TMP3D(:,:,3,JKGLO)
-        TEND_TMP(JKGLO)%CLD => TMP3D(:,:,4:,JKGLO)
-    END DO
-    CALL FSET%DATA(32, TMP3D)
-    DO JKGLO = 1, FSPACE%NBLKS()
-        TEND_LOC(JKGLO)%T   => TMP3D(:,:,1,JKGLO)
-        TEND_LOC(JKGLO)%A   => TMP3D(:,:,2,JKGLO)
-        TEND_LOC(JKGLO)%Q   => TMP3D(:,:,3,JKGLO)
-        TEND_LOC(JKGLO)%CLD => TMP3D(:,:,4:,JKGLO)
-    END DO
 
-    CALL FSET%DATA(33, PFSQLF)
-    CALL FSET%DATA(34, PFSQIF)
-    CALL FSET%DATA(35, PFCQLNG)
-    CALL FSET%DATA(36, PFCQNNG)
-    CALL FSET%DATA(37, PFSQRF)
-    CALL FSET%DATA(38, PFSQSF)
-    CALL FSET%DATA(39, PFCQRNG)
-    CALL FSET%DATA(40, PFCQSNG)
-    CALL FSET%DATA(41, PFSQLTUR)
-    CALL FSET%DATA(42, PFSQITUR)
-    CALL FSET%DATA(43, PFPLSL)
-    CALL FSET%DATA(44, PFPLSN)
-    CALL FSET%DATA(45, PFHPSL)
-    CALL FSET%DATA(46, PFHPSN)
-    CALL FSET%DATA(47, PCOVPTOT)
-    CALL FSET%DATA(48, PRAINFRAC_TOPRFZ)
-
-    DO JKGLO = 1, 48
-!        IF (JKGLO > 29 .OR. JKGLO < 33) CONTINUE
-        FIELD = FSET%FIELD(JKGLO)
-        CALL FIELD%UPDATE_DEVICE()
-    END DO
-
-!!    !$omp parallel default(shared) private(JKGLO,IBL,ICEND,TID,energy,power,FBLOCK) &
-!!    !$omp& num_threads(NUMOMP)
-!$acc data deviceptr(PT, PQ,TEND_CML,TEND_LOC,&
-!$acc & TEND_TMP, PVFA, PVFL, PVFI, &
-!$acc & PDYNA, PDYNL, PDYNI, PHRSW,    PHRLW,&
-!$acc & PVERVEL,  PAP,      PAPH,&
-!$acc & PLSM,       LDCUM,      KTYPE, &
-!$acc & PLU,      PLUDE,    PSNDE,    PMFU,     PMFD,&
-!$acc & PA,       PCLV,   PSUPSAT,&
-!$acc & PLCRIT_AER,PICRIT_AER,&
-!$acc & PRE_ICE, PCCN,     PNICE,&
-!$acc & PCOVPTOT, PRAINFRAC_TOPRFZ,&
-!$acc & PFSQLF,   PFSQIF ,  PFCQNNG,  PFCQLNG,&
-!$acc & PFSQRF,   PFSQSF ,  PFCQRNG,  PFCQSNG,&
-!$acc & PFSQLTUR, PFSQITUR , &
-!$acc & PFPLSL,   PFPLSN,   PFHPSL,   PFHPSN)
+!$acc data copyin(YRECLDP) present(&
+!$acc & PLCRIT_AER, PICRIT_AER, PRE_ICE,    PCCN,       PNICE, &
+!$acc & PT,         PQ,         PVFA,       PVFL,       PVFI,  &
+!$acc & PDYNA,      PDYNL,      PDYNI,      PHRSW,      PHRLW, &
+!$acc & PVERVEL,    PAP,        PLU,        PLUDE,      PSNDE, &
+!$acc & PMFU,       PMFD,       PA,         PSUPSAT,           &
+!$acc & TENDENCY_CML_T, TENDENCY_CML_A, TENDENCY_CML_Q, &
+!$acc & TENDENCY_TMP_T, TENDENCY_TMP_A, TENDENCY_TMP_Q, &
+!$acc & PLSM,       LDCUM,      KTYPE,      PAPH,       PCLV,     &
+!$acc & TENDENCY_CML_CLD,           TENDENCY_TMP_CLD, &
+!$acc & PFSQLF,     PFSQIF,     PFCQNNG,    PFCQLNG,    PFSQRF,   &
+!$acc & PFSQSF ,    PFCQRNG,    PFCQSNG,    PFSQLTUR,   PFSQITUR, &
+!$acc & PFPLSL,     PFPLSN,     PFHPSL,     PFHPSN,     PCOVPTOT, &
+!$acc & TENDENCY_LOC_T, TENDENCY_LOC_A, TENDENCY_LOC_Q, &
+!$acc & PRAINFRAC_TOPRFZ, TENDENCY_LOC_CLD )
 
     ! Local timer for each thread
-    !TID = GET_THREAD_NUM()
-    !CALL TIMER%THREAD_START(TID)
+    TID = GET_THREAD_NUM()
+    CALL TIMER%THREAD_START(TID)
 
-!!    !$omp do schedule(runtime) reduction(+:power_total,power_count) reduction(max:power_max)
 !$acc parallel loop gang vector_length(NPROMA)
     DO JKGLO=1,NGPTOT,NPROMA
        IBL=(JKGLO-1)/NPROMA+1
        ICEND=MIN(NPROMA,NGPTOT-JKGLO+1)
 
-         ! get block views
-         !call FBLOCK%GET_BLOCK(FSET, IBL)
+       CALL CLOUDSC_SCC &
+        & (1, ICEND, NPROMA, NLEV, PTSPHY,&
+        & PT(:,:,IBL), PQ(:,:,IBL), &
+        & TENDENCY_TMP_T(:,:,IBL), TENDENCY_TMP_Q(:,:,IBL), TENDENCY_TMP_A(:,:,IBL), TENDENCY_TMP_CLD(:,:,:,IBL), &
+        & TENDENCY_LOC_T(:,:,IBL), TENDENCY_LOC_Q(:,:,IBL), TENDENCY_LOC_A(:,:,IBL), TENDENCY_LOC_CLD(:,:,:,IBL), &
+        & PVFA(:,:,IBL), PVFL(:,:,IBL), PVFI(:,:,IBL), PDYNA(:,:,IBL), PDYNL(:,:,IBL), PDYNI(:,:,IBL), &
+        & PHRSW(:,:,IBL),    PHRLW(:,:,IBL),&
+        & PVERVEL(:,:,IBL),  PAP(:,:,IBL),      PAPH(:,:,IBL),&
+        & PLSM(:,IBL),       LDCUM(:,IBL),      KTYPE(:,IBL), &
+        & PLU(:,:,IBL),      PLUDE(:,:,IBL),    PSNDE(:,:,IBL),    PMFU(:,:,IBL),     PMFD(:,:,IBL),&
+        !---prognostic fields
+        & PA(:,:,IBL),       PCLV(:,:,:,IBL),   PSUPSAT(:,:,IBL),&
+        !-- arrays for aerosol-cloud interactions
+        & PLCRIT_AER(:,:,IBL),PICRIT_AER(:,:,IBL),&
+        & PRE_ICE(:,:,IBL),&
+        & PCCN(:,:,IBL),     PNICE(:,:,IBL),&
+        !---diagnostic output
+        & PCOVPTOT(:,:,IBL), PRAINFRAC_TOPRFZ(:,IBL),&
+        !---resulting fluxes
+        & PFSQLF(:,:,IBL),   PFSQIF (:,:,IBL),  PFCQNNG(:,:,IBL),  PFCQLNG(:,:,IBL),&
+        & PFSQRF(:,:,IBL),   PFSQSF (:,:,IBL),  PFCQRNG(:,:,IBL),  PFCQSNG(:,:,IBL),&
+        & PFSQLTUR(:,:,IBL), PFSQITUR (:,:,IBL), &
+        & PFPLSL(:,:,IBL),   PFPLSN(:,:,IBL),   PFHPSL(:,:,IBL),   PFHPSN(:,:,IBL),&
+        & YRECLDP=YRECLDP)
 
-         !-- These were uninitialized : meaningful only when we compare error differences
-         !FBLOCK%PCOVPTOT(:,:) = 0.0_JPRB
-         !FBLOCK%TENDENCY_LOC%cld(:,:,NCLV) = 0.0_JPRB
-
-          CALL CLOUDSC &
-                & (1, ICEND, NPROMA, NLEV, PTSPHY,&
-                & PT(:,:,IBL), PQ(:,:,IBL), &
-                & TEND_CML(IBL), TEND_TMP(IBL), TEND_LOC(IBL), &
-                & PVFA(:,:,IBL), PVFL(:,:,IBL), PVFI(:,:,IBL), PDYNA(:,:,IBL), PDYNL(:,:,IBL), PDYNI(:,:,IBL), &
-                & PHRSW(:,:,IBL),    PHRLW(:,:,IBL),&
-                & PVERVEL(:,:,IBL),  PAP(:,:,IBL),      PAPH(:,:,IBL),&
-                & PLSM(:,IBL),       LDCUM(:,IBL),      KTYPE(:,IBL), &
-                & PLU(:,:,IBL),      PLUDE(:,:,IBL),    PSNDE(:,:,IBL),    PMFU(:,:,IBL),     PMFD(:,:,IBL),&
-                !---prognostic fields
-                & PA(:,:,IBL),       PCLV(:,:,:,IBL),   PSUPSAT(:,:,IBL),&
-                !-- arrays for aerosol-cloud interactions
-                & PLCRIT_AER(:,:,IBL),PICRIT_AER(:,:,IBL),&
-                & PRE_ICE(:,:,IBL),&
-                & PCCN(:,:,IBL),     PNICE(:,:,IBL),&
-                !---diagnostic output
-                & PCOVPTOT(:,:,IBL), PRAINFRAC_TOPRFZ(:,IBL),&
-                !---resulting fluxes
-                & PFSQLF(:,:,IBL),   PFSQIF (:,:,IBL),  PFCQNNG(:,:,IBL),  PFCQLNG(:,:,IBL),&
-                & PFSQRF(:,:,IBL),   PFSQSF (:,:,IBL),  PFCQRNG(:,:,IBL),  PFCQSNG(:,:,IBL),&
-                & PFSQLTUR(:,:,IBL), PFSQITUR (:,:,IBL), &
-                & PFPLSL(:,:,IBL),   PFPLSN(:,:,IBL),   PFHPSL(:,:,IBL),   PFHPSN(:,:,IBL),&
-                & KFLDX )
-
-          !--- end of a future plan to replace the call to CLOUDSC ------ 
-
-         IF (LEC_PMON) THEN
-           ! Sample power consuption
-           IF (MOD(IBL, 100) == 0) THEN
-             !CALL EC_PMON(ENERGY, POWER)
-             POWER_MAX = MAX(POWER_MAX, POWER)
-             POWER_TOTAL = POWER_TOTAL + POWER
-             POWER_COUNT = POWER_COUNT + 1
-           END IF
-         END IF
-
-         ! Log number of columns processed by this thread
-         !CALL TIMER%THREAD_LOG(TID, IGPC=ICEND)
       ENDDO
 !$acc end parallel loop
-
-      !-- The "nowait" is here to get correct local timings (tloc) per thread
-      !   i.e. we should not wait for slowest thread to finish before measuring tloc
-!!     !$omp end do nowait
+!$acc end data
 
       CALL TIMER%THREAD_END(TID)
 
-!!      !$omp end parallel
-!$acc end data
+      ! copy back to host
 
       CALL TIMER%END()
 
-      CALL TIMER%PRINT_PERFORMANCE(NPROMA, NGPBLKS, NGPTOT)
+      ! On GPUs, adding block-level column totals is cumbersome and
+      ! error prone, and of little value due to the large number of
+      ! processing "thread teams". Instead we register the total here.
+      CALL TIMER%THREAD_LOG(TID=TID, IGPC=NGPTOT)
 
-      IF (LEC_PMON) THEN
-        print *, "Power usage (sampled):: max: ", POWER_MAX, "avg:", &
-         & (REAL(POWER_TOTAL, KIND=JPRD) / REAL(POWER_COUNT, KIND=JPRD)), &
-         & "count:", POWER_COUNT
-      END IF
+      CALL TIMER%PRINT_PERFORMANCE(NPROMA, NGPBLKS, NGPTOT)
 
       CALL FIELD%FINAL()
       CALL FSPACE%FINAL()
