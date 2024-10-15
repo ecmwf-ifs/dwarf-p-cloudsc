a88881951b06f13f13e98e6563d322d0018f8643
Slavko Brdar <slavko.brdar@ecmwf.int>
2024-10-15 16:50:58 +0300

    move kernel into a subroutine (needed for acc deviceptr on Lumi)

--- src/cloudsc_gpu_atlas/cloudsc_driver_scc_mod.F90
+++ src/cloudsc_gpu_atlas/cloudsc_driver_scc_mod.F90
@@ -23,6 +23,140 @@ MODULE CLOUDSC_DRIVER_SCC_MOD
 
 CONTAINS
 
+  SUBROUTINE CLOUDSC_KERNEL( NGPTOT, NLEV, NPROMA, PTSPHY, &
+      & PLCRIT_AER, PICRIT_AER, PRE_ICE,    PCCN,       PNICE, &
+      & PT,         PQ,         PVFA,       PVFL,       PVFI,  & 
+      & PDYNA,      PDYNL,      PDYNI,      PHRSW,      PHRLW, &
+      & PVERVEL,    PAP,        PLU,        PLUDE,      PSNDE, &
+      & PMFU,       PMFD,       PA,         PSUPSAT,           &
+      & TENDENCY_CML_T, TENDENCY_CML_A, TENDENCY_CML_Q, &
+      & TENDENCY_TMP_T, TENDENCY_TMP_A, TENDENCY_TMP_Q, &
+      & PLSM,       LDCUM,      KTYPE,      PAPH,       PCLV,     &
+      & TENDENCY_CML_CLD,           TENDENCY_TMP_CLD, &
+      & PFSQLF,     PFSQIF,     PFCQNNG,    PFCQLNG,    PFSQRF,   &
+      & PFSQSF ,    PFCQRNG,    PFCQSNG,    PFSQLTUR,   PFSQITUR, &
+      & PFPLSL,     PFPLSN,     PFHPSL,     PFHPSN,     PCOVPTOT, &
+      & TENDENCY_LOC_T, TENDENCY_LOC_A, TENDENCY_LOC_Q, &
+      & PRAINFRAC_TOPRFZ, TENDENCY_LOC_CLD )
+    INTEGER(KIND=JPIM), INTENT(IN) :: NGPTOT
+    INTEGER(KIND=JPIM), INTENT(IN) :: NLEV
+    INTEGER(KIND=JPIM), INTENT(IN) :: NPROMA
+    REAL(KIND=JPRB), INTENT(IN)   :: PTSPHY       ! Physics timestep
+    REAL(KIND=JPRB), CONTIGUOUS  :: PLCRIT_AER(:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: PICRIT_AER(:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: PRE_ICE(:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: PCCN(:,:,:)     ! liquid cloud condensation nuclei
+    REAL(KIND=JPRB), CONTIGUOUS  :: PNICE(:,:,:)    ! ice number concentration (cf. CCN)
+    REAL(KIND=JPRB), CONTIGUOUS  :: PT(:,:,:)       ! T at start of callpar
+    REAL(KIND=JPRB), CONTIGUOUS  :: PQ(:,:,:)       ! Q at start of callpar
+    REAL(KIND=JPRB), CONTIGUOUS  :: PVFA(:,:,:)     ! CC from VDF scheme
+    REAL(KIND=JPRB), CONTIGUOUS  :: PVFL(:,:,:)     ! Liq from VDF scheme
+    REAL(KIND=JPRB), CONTIGUOUS  :: PVFI(:,:,:)     ! Ice from VDF scheme
+    REAL(KIND=JPRB), CONTIGUOUS  :: PDYNA(:,:,:)    ! CC from Dynamics
+    REAL(KIND=JPRB), CONTIGUOUS  :: PDYNL(:,:,:)    ! Liq from Dynamics
+    REAL(KIND=JPRB), CONTIGUOUS  :: PDYNI(:,:,:)    ! Liq from Dynamics
+    REAL(KIND=JPRB), CONTIGUOUS  :: PHRSW(:,:,:)    ! Short-wave heating rate
+    REAL(KIND=JPRB), CONTIGUOUS  :: PHRLW(:,:,:)    ! Long-wave heating rate
+    REAL(KIND=JPRB), CONTIGUOUS  :: PVERVEL(:,:,:)  ! Vertical velocity
+    REAL(KIND=JPRB), CONTIGUOUS  :: PAP(:,:,:)      ! Pressure on full levels
+    REAL(KIND=JPRB), CONTIGUOUS  :: PLU(:,:,:)      ! Conv. condensate
+    REAL(KIND=JPRB), CONTIGUOUS  :: PLUDE(:,:,:)    ! Conv. detrained water
+    REAL(KIND=JPRB), CONTIGUOUS  :: PSNDE(:,:,:)    ! Conv. detrained snow
+    REAL(KIND=JPRB), CONTIGUOUS  :: PMFU(:,:,:)     ! Conv. mass flux up
+    REAL(KIND=JPRB), CONTIGUOUS  :: PMFD(:,:,:)     ! Conv. mass flux down
+    REAL(KIND=JPRB), CONTIGUOUS  :: PA(:,:,:)       ! Original Cloud fraction (t)
+    REAL(KIND=JPRB), CONTIGUOUS  :: PSUPSAT(:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: TENDENCY_CML_T(:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: TENDENCY_CML_A(:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: TENDENCY_CML_Q(:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: TENDENCY_TMP_T(:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: TENDENCY_TMP_A(:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: TENDENCY_TMP_Q(:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: PLSM(:,:)       ! Land fraction (0-1)
+    LOGICAL, CONTIGUOUS          :: LDCUM(:,:)      ! Convection active
+    INTEGER(KIND=JPIM),CONTIGUOUS:: KTYPE(:,:)      ! Convection type 0,1,2
+    REAL(KIND=JPRB), CONTIGUOUS  :: PAPH(:,:,:)     ! Pressure on half levels
+    REAL(KIND=JPRB), CONTIGUOUS  :: PCLV(:,:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: TENDENCY_CML_CLD(:,:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: TENDENCY_TMP_CLD(:,:,:,:)
+
+    ! output variables
+    ! Note: flux diagnostics for DDH budget are named P*
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFSQLF(:,:,:)    ! Flux of liquid
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFSQIF(:,:,:)    ! Flux of ice
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFCQLNG(:,:,:)   ! -ve corr for liq
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFCQNNG(:,:,:)   ! -ve corr for ice
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFSQRF(:,:,:)    ! Flux diagnostics
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFSQSF(:,:,:)    !    for DDH, generic
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFCQRNG(:,:,:)   ! rain
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFCQSNG(:,:,:)   ! snow
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFSQLTUR(:,:,:)  ! liquid flux due to VDF
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFSQITUR(:,:,:)  ! ice flux due to VDF
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFPLSL(:,:,:)    ! liq+rain sedim flux
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFPLSN(:,:,:)    ! ice+snow sedim flux
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFHPSL(:,:,:)    ! Enthalpy flux for liq
+    REAL(KIND=JPRB), CONTIGUOUS  :: PFHPSN(:,:,:)    ! ice number concentration (cf. CCN)
+    REAL(KIND=JPRB), CONTIGUOUS  :: PCOVPTOT(:,:,:)    ! Precip fraction
+    REAL(KIND=JPRB), CONTIGUOUS  :: TENDENCY_LOC_T(:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: TENDENCY_LOC_A(:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: TENDENCY_LOC_Q(:,:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: PRAINFRAC_TOPRFZ(:,:)
+    REAL(KIND=JPRB), CONTIGUOUS  :: TENDENCY_LOC_CLD(:,:,:,:)
+
+    INTEGER :: JKGLO, IBL, ICEND
+
+!$acc data copyin(YRECLDP) deviceptr(&
+!$acc & PLCRIT_AER, PICRIT_AER, PRE_ICE,    PCCN,       PNICE, &
+!$acc & PT,         PQ,         PVFA,       PVFL,       PVFI,  &
+!$acc & PDYNA,      PDYNL,      PDYNI,      PHRSW,      PHRLW, &
+!$acc & PVERVEL,    PAP,        PLU,        PLUDE,      PSNDE, &
+!$acc & PMFU,       PMFD,       PA,         PSUPSAT,           &
+!$acc & TENDENCY_CML_T, TENDENCY_CML_A, TENDENCY_CML_Q, &
+!$acc & TENDENCY_TMP_T, TENDENCY_TMP_A, TENDENCY_TMP_Q, &
+!$acc & PLSM,       LDCUM,      KTYPE,      PAPH,       PCLV,     &
+!$acc & TENDENCY_CML_CLD,           TENDENCY_TMP_CLD, &
+!$acc & PFSQLF,     PFSQIF,     PFCQNNG,    PFCQLNG,    PFSQRF,   &
+!$acc & PFSQSF ,    PFCQRNG,    PFCQSNG,    PFSQLTUR,   PFSQITUR, &
+!$acc & PFPLSL,     PFPLSN,     PFHPSL,     PFHPSN,     PCOVPTOT, &
+!$acc & TENDENCY_LOC_T, TENDENCY_LOC_A, TENDENCY_LOC_Q, &
+!$acc & PRAINFRAC_TOPRFZ, TENDENCY_LOC_CLD )
+
+!$acc parallel loop gang vector_length(NPROMA)
+    DO JKGLO=1,NGPTOT,NPROMA
+       IBL=(JKGLO-1)/NPROMA+1
+       ICEND=MIN(NPROMA,NGPTOT-JKGLO+1)
+
+       CALL CLOUDSC_SCC &
+        & (1, ICEND, NPROMA, NLEV, PTSPHY,&
+        & PT(:,:,IBL), PQ(:,:,IBL), &
+        & TENDENCY_TMP_T(:,:,IBL), TENDENCY_TMP_Q(:,:,IBL), TENDENCY_TMP_A(:,:,IBL), TENDENCY_TMP_CLD(:,:,:,IBL), &
+        & TENDENCY_LOC_T(:,:,IBL), TENDENCY_LOC_Q(:,:,IBL), TENDENCY_LOC_A(:,:,IBL), TENDENCY_LOC_CLD(:,:,:,IBL), &
+        & PVFA(:,:,IBL), PVFL(:,:,IBL), PVFI(:,:,IBL), PDYNA(:,:,IBL), PDYNL(:,:,IBL), PDYNI(:,:,IBL), &
+        & PHRSW(:,:,IBL),    PHRLW(:,:,IBL),&
+        & PVERVEL(:,:,IBL),  PAP(:,:,IBL),      PAPH(:,:,IBL),&
+        & PLSM(:,IBL),       LDCUM(:,IBL),      KTYPE(:,IBL), &
+        & PLU(:,:,IBL),      PLUDE(:,:,IBL),    PSNDE(:,:,IBL),    PMFU(:,:,IBL),     PMFD(:,:,IBL),&
+        !---prognostic fields
+        & PA(:,:,IBL),       PCLV(:,:,:,IBL),   PSUPSAT(:,:,IBL),&
+        !-- arrays for aerosol-cloud interactions
+        & PLCRIT_AER(:,:,IBL),PICRIT_AER(:,:,IBL),&
+        & PRE_ICE(:,:,IBL),&
+        & PCCN(:,:,IBL),     PNICE(:,:,IBL),&
+        !---diagnostic output
+        & PCOVPTOT(:,:,IBL), PRAINFRAC_TOPRFZ(:,IBL),&
+        !---resulting fluxes
+        & PFSQLF(:,:,IBL),   PFSQIF (:,:,IBL),  PFCQNNG(:,:,IBL),  PFCQLNG(:,:,IBL),&
+        & PFSQRF(:,:,IBL),   PFSQSF (:,:,IBL),  PFCQRNG(:,:,IBL),  PFCQSNG(:,:,IBL),&
+        & PFSQLTUR(:,:,IBL), PFSQITUR (:,:,IBL), &
+        & PFPLSL(:,:,IBL),   PFPLSN(:,:,IBL),   PFHPSL(:,:,IBL),   PFHPSN(:,:,IBL),&
+        & YRECLDP=YRECLDP)
+
+      ENDDO
+!$acc end parallel loop
+!$acc end data
+  
+  END SUBROUTINE CLOUDSC_KERNEL
+
   SUBROUTINE CLOUDSC_DRIVER(FSET, NUMOMP, NGPTOTG, KFLDX, PTSPHY)
     ! Driver routine that performans the parallel NPROMA-blocking and
     ! invokes the CLOUDSC kernel
@@ -187,59 +321,26 @@ CONTAINS
     CALL FSET%DEVICE_DATA(56, PRAINFRAC_TOPRFZ)
     CALL FSET%DEVICE_DATA(57, TENDENCY_LOC_CLD)
 
-!$acc data copyin(YRECLDP) deviceptr(&
-!$acc & PLCRIT_AER, PICRIT_AER, PRE_ICE,    PCCN,       PNICE, &
-!$acc & PT,         PQ,         PVFA,       PVFL,       PVFI,  &
-!$acc & PDYNA,      PDYNL,      PDYNI,      PHRSW,      PHRLW, &
-!$acc & PVERVEL,    PAP,        PLU,        PLUDE,      PSNDE, &
-!$acc & PMFU,       PMFD,       PA,         PSUPSAT,           &
-!$acc & TENDENCY_CML_T, TENDENCY_CML_A, TENDENCY_CML_Q, &
-!$acc & TENDENCY_TMP_T, TENDENCY_TMP_A, TENDENCY_TMP_Q, &
-!$acc & PLSM,       LDCUM,      KTYPE,      PAPH,       PCLV,     &
-!$acc & TENDENCY_CML_CLD,           TENDENCY_TMP_CLD, &
-!$acc & PFSQLF,     PFSQIF,     PFCQNNG,    PFCQLNG,    PFSQRF,   &
-!$acc & PFSQSF ,    PFCQRNG,    PFCQSNG,    PFSQLTUR,   PFSQITUR, &
-!$acc & PFPLSL,     PFPLSN,     PFHPSL,     PFHPSN,     PCOVPTOT, &
-!$acc & TENDENCY_LOC_T, TENDENCY_LOC_A, TENDENCY_LOC_Q, &
-!$acc & PRAINFRAC_TOPRFZ, TENDENCY_LOC_CLD )
-
     ! Local timer for each thread
     TID = GET_THREAD_NUM()
     CALL TIMER%THREAD_START(TID)
 
-!$acc parallel loop gang vector_length(NPROMA)
-    DO JKGLO=1,NGPTOT,NPROMA
-       IBL=(JKGLO-1)/NPROMA+1
-       ICEND=MIN(NPROMA,NGPTOT-JKGLO+1)
-
-       CALL CLOUDSC_SCC &
-        & (1, ICEND, NPROMA, NLEV, PTSPHY,&
-        & PT(:,:,IBL), PQ(:,:,IBL), &
-        & TENDENCY_TMP_T(:,:,IBL), TENDENCY_TMP_Q(:,:,IBL), TENDENCY_TMP_A(:,:,IBL), TENDENCY_TMP_CLD(:,:,:,IBL), &
-        & TENDENCY_LOC_T(:,:,IBL), TENDENCY_LOC_Q(:,:,IBL), TENDENCY_LOC_A(:,:,IBL), TENDENCY_LOC_CLD(:,:,:,IBL), &
-        & PVFA(:,:,IBL), PVFL(:,:,IBL), PVFI(:,:,IBL), PDYNA(:,:,IBL), PDYNL(:,:,IBL), PDYNI(:,:,IBL), &
-        & PHRSW(:,:,IBL),    PHRLW(:,:,IBL),&
-        & PVERVEL(:,:,IBL),  PAP(:,:,IBL),      PAPH(:,:,IBL),&
-        & PLSM(:,IBL),       LDCUM(:,IBL),      KTYPE(:,IBL), &
-        & PLU(:,:,IBL),      PLUDE(:,:,IBL),    PSNDE(:,:,IBL),    PMFU(:,:,IBL),     PMFD(:,:,IBL),&
-        !---prognostic fields
-        & PA(:,:,IBL),       PCLV(:,:,:,IBL),   PSUPSAT(:,:,IBL),&
-        !-- arrays for aerosol-cloud interactions
-        & PLCRIT_AER(:,:,IBL),PICRIT_AER(:,:,IBL),&
-        & PRE_ICE(:,:,IBL),&
-        & PCCN(:,:,IBL),     PNICE(:,:,IBL),&
-        !---diagnostic output
-        & PCOVPTOT(:,:,IBL), PRAINFRAC_TOPRFZ(:,IBL),&
-        !---resulting fluxes
-        & PFSQLF(:,:,IBL),   PFSQIF (:,:,IBL),  PFCQNNG(:,:,IBL),  PFCQLNG(:,:,IBL),&
-        & PFSQRF(:,:,IBL),   PFSQSF (:,:,IBL),  PFCQRNG(:,:,IBL),  PFCQSNG(:,:,IBL),&
-        & PFSQLTUR(:,:,IBL), PFSQITUR (:,:,IBL), &
-        & PFPLSL(:,:,IBL),   PFPLSN(:,:,IBL),   PFHPSL(:,:,IBL),   PFHPSN(:,:,IBL),&
-        & YRECLDP=YRECLDP)
-
-      ENDDO
-!$acc end parallel loop
-!$acc end data
+    CALL CLOUDSC_KERNEL( NGPTOT, NLEV, NPROMA, PTSPHY, &
+      & PLCRIT_AER, PICRIT_AER, PRE_ICE,    PCCN,       PNICE, &
+      & PT,         PQ,         PVFA,       PVFL,       PVFI,  & 
+      & PDYNA,      PDYNL,      PDYNI,      PHRSW,      PHRLW, &
+      & PVERVEL,    PAP,        PLU,        PLUDE,      PSNDE, &
+      & PMFU,       PMFD,       PA,         PSUPSAT,           &
+      & TENDENCY_CML_T, TENDENCY_CML_A, TENDENCY_CML_Q, &
+      & TENDENCY_TMP_T, TENDENCY_TMP_A, TENDENCY_TMP_Q, &
+      & PLSM,       LDCUM,      KTYPE,      PAPH,       PCLV,     &
+      & TENDENCY_CML_CLD,           TENDENCY_TMP_CLD, &
+      & PFSQLF,     PFSQIF,     PFCQNNG,    PFCQLNG,    PFSQRF,   &
+      & PFSQSF ,    PFCQRNG,    PFCQSNG,    PFSQLTUR,   PFSQITUR, &
+      & PFPLSL,     PFPLSN,     PFHPSL,     PFHPSN,     PCOVPTOT, &
+      & TENDENCY_LOC_T, TENDENCY_LOC_A, TENDENCY_LOC_Q, &
+      & PRAINFRAC_TOPRFZ, TENDENCY_LOC_CLD &
+    )
 
       CALL TIMER%THREAD_END(TID)
 
