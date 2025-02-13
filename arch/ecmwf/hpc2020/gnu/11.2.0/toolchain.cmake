c309fb0f1f8616ea4ff1335e8128a1ee3428c4e8
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2025-02-14 00:24:00 +0100

    Clean-up hpc2020 archs

--- arch/ecmwf/hpc2020/gnu/11.2.0/toolchain.cmake
+++ arch/ecmwf/hpc2020/gnu/11.2.0/toolchain.cmake
@@ -7,24 +7,6 @@
 # nor does it submit to any jurisdiction.
 
 ####################################################################
-# COMPILER
-####################################################################
-
-set( ECBUILD_FIND_MPI ON )
-
-####################################################################
-# OpenMP FLAGS
-####################################################################
-
-# set( OpenMP_Fortran_FLAGS       "-fopenmp -foffload=nvptx-none" )
-
-####################################################################
-# OpenAcc FLAGS
-####################################################################
-
-# set( OpenACC_Fortran_FLAGS "-fopenacc -foffload=nvptx-none" )
-
-####################################################################
 # Compiler FLAGS
 ####################################################################
 
@@ -37,6 +19,3 @@ set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -fno-second-underscore")
 set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -ffast-math")
 set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -fno-unsafe-math-optimizations")
 set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -march=znver2")
-
-# This is dangerous! But GNU 10+ complains about argument mismatch for MPI routines
-set(ECBUILD_Fortran_FLAGS "${ECBUILD_Fortran_FLAGS} -fallow-argument-mismatch")
