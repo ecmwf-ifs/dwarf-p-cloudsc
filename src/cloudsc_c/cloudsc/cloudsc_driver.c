bac9b23bbe6bfdb92dca21932792c5e4e53c2b5d
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2025-02-14 13:17:36 +0200

    Fix various compiler warnings

--- src/cloudsc_c/cloudsc/cloudsc_driver.c
+++ src/cloudsc_c/cloudsc/cloudsc_driver.c
@@ -241,7 +241,7 @@ void cloudsc_driver(int numthreads, int numcols, int nproma) {
 
   printf("     NUMOMP=%d, NGPTOT=%d, NPROMA=%d, NGPBLKS=%d\n", numthreads, numcols, nproma, nblocks);
   printf(" Reference MFLOP count for 100 columns : %12.8f\n", 1.0e-06 * zhpm);
-  printf(" %+10s%+10s%+10s%+10s%+10s %+4s : %+10s%+10s%+10s\n",
+  printf(" %10s%10s%10s%10s%10s %4s : %10s%10s%10s\n",
     "NUMOMP", "NGPTOT", "#GP-cols", "#BLKS", "NPROMA", "tid#", "Time(msec)", "MFlops/s", "col/s");
   double zfrac, zmflops, zthrput;
   for (int t = 0; t < numthreads; t++) {
