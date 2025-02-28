8fb5187797750262895da18db84ef2a7d855ed2c
Slavko Brdar <slavko.brdar@ecmwf.int>
2025-02-28 16:16:19 +0000

    make compile again on Atos and nvhpc 22.11

--- src/cloudsc_gpu_atlas/dwarf_cloudsc_atlas.F90
+++ src/cloudsc_gpu_atlas/dwarf_cloudsc_atlas.F90
@@ -11,13 +11,13 @@ PROGRAM DWARF_CLOUDSC
 
 USE PARKIND1, ONLY: JPIM
 USE CLOUDSC_MPI_MOD, ONLY: CLOUDSC_MPI_INIT, CLOUDSC_MPI_END, NUMPROC, IRANK
-USE CLOUDSC_GLOBAL_ATLAS_STATE_MOD, ONLY: CLOUDSC_GLOBAL_ATLAS_STATE, GET_ENV_INT
+USE CLOUDSC_GLOBAL_ATLAS_STATE_MOD, ONLY: CLOUDSC_GLOBAL_ATLAS_STATE
 USE CLOUDSC_DRIVER_SCC_MOD, ONLY: CLOUDSC_DRIVER
 
 USE ATLAS_MODULE
 USE, INTRINSIC :: ISO_C_BINDING
 USE ATLAS_FUNCTIONSPACE_BLOCKSTRUCTUREDCOLUMNS_MODULE
-!USE ATLAS_MULTIFIELD_MODULE
+USE ATLAS_MULTIFIELD_MODULE
 
 #ifdef _OPENMP
 USE OMP_LIB
@@ -41,65 +41,78 @@ TYPE(ATLAS_TRACE) :: TRACE
 
 #include "abor1.intfb.h"
 
-IARGS = COMMAND_ARGUMENT_COUNT()
+  IARGS = COMMAND_ARGUMENT_COUNT()
 
-! Get the number of OpenMP threads to use for the benchmark
-if (IARGS >= 1) then
-  CALL GET_COMMAND_ARGUMENT(1, CLARG, LENARG)
-  READ(CLARG(1:LENARG),*) NUMOMP
-  if (NUMOMP <= 0) then
-#ifdef _OPENMP
-    NUMOMP = OMP_GET_MAX_THREADS()
-#else
-    ! if arg is 0 or negative, and OpenMP disabled; defaults to 1
-    NUMOMP = 1
-#endif
+  ! Get the number of OpenMP threads to use for the benchmark
+  if (IARGS >= 1) then
+    CALL GET_COMMAND_ARGUMENT(1, CLARG, LENARG)
+    READ(CLARG(1:LENARG),*) NUMOMP
+    if (NUMOMP <= 0) then
+  #ifdef _OPENMP
+      NUMOMP = OMP_GET_MAX_THREADS()
+  #else
+      ! if arg is 0 or negative, and OpenMP disabled; defaults to 1
+      NUMOMP = 1
+  #endif
+    end if
   end if
-end if
 
-! Initialize MPI environment
-CALL CLOUDSC_MPI_INIT(NUMOMP)
-CALL ATLAS_LIBRARY%INITIALISE()
-TRACE = ATLAS_TRACE("dwarf_cloudsc_atlas.F90",__LINE__,"program")
+  ! Initialize MPI environment
+  CALL CLOUDSC_MPI_INIT(NUMOMP)
+  CALL ATLAS_LIBRARY%INITIALISE()
+  TRACE = ATLAS_TRACE("dwarf_cloudsc_atlas.F90",__LINE__,"program")
 
-! Get total number of grid points (NGPTOTG) with which to run the benchmark
-IF (IARGS >= 2) THEN
-  CALL GET_COMMAND_ARGUMENT(2, CLARG, LENARG)
-  READ(CLARG(1:LENARG),*) NGPTOTG
-END IF
+  ! Get total number of grid points (NGPTOTG) with which to run the benchmark
+  IF (IARGS >= 2) THEN
+    CALL GET_COMMAND_ARGUMENT(2, CLARG, LENARG)
+    READ(CLARG(1:LENARG),*) NGPTOTG
+  END IF
 
-! Get the block size (NPROMA) for which to run the benchmark  
-IF (IARGS >= 3) THEN
-  CALL GET_COMMAND_ARGUMENT(3, CLARG, LENARG)
-  READ(CLARG(1:LENARG),*) NPROMA
-ENDIF
+  ! Get the block size (NPROMA) for which to run the benchmark  
+  IF (IARGS >= 3) THEN
+    CALL GET_COMMAND_ARGUMENT(3, CLARG, LENARG)
+    READ(CLARG(1:LENARG),*) NPROMA
+  ENDIF
 
-! timing of memory pool allocations
-FSET = ATLAS_FIELDSET()
-CALL GLOBAL_ATLAS_STATE%LOAD(FSET, FSPACE, NPROMA, NGPTOTG)
-write(0,*) " ### Ignore the above timer, it is timing the one-time memory pool allocation"
+  ! timing of memory pool allocations
+  FSET = ATLAS_FIELDSET()
+  CALL GLOBAL_ATLAS_STATE%LOAD(FSET, FSPACE, NPROMA, NGPTOTG)
+  write(0,*) " ### Ignore the above timer, it is timing the one-time memory pool allocation"
 
-FSET = ATLAS_FIELDSET()
-CALL GLOBAL_ATLAS_STATE%LOAD(FSET, FSPACE, NPROMA, NGPTOTG)
-write(0,*) " ### Above timer is host allocation"
+  FSET = ATLAS_FIELDSET()
+  CALL GLOBAL_ATLAS_STATE%LOAD(FSET, FSPACE, NPROMA, NGPTOTG)
+  write(0,*) " ### Above timer is host allocation"
 
-CALL GET_ENV_INT("NITER",NITER)
+  CALL GET_ENV_INT("NITER", NITER)
 
-! Call the driver to perform the parallel loop over our kernel
-DO  JITER = 1, NITER
-  write(0,'(A,I0,A,I0)') "### ITERATION ", JITER, '/', NITER
-  CALL CLOUDSC_DRIVER(FSET, NUMOMP, NGPTOTG, GLOBAL_ATLAS_STATE%KFLDX, GLOBAL_ATLAS_STATE%PTSPHY)
-ENDDO
+  ! Call the driver to perform the parallel loop over our kernel
+  DO  JITER = 1, NITER
+    write(0,'(A,I0,A,I0)') "### ITERATION ", JITER, '/', NITER
+    CALL CLOUDSC_DRIVER(FSET, NUMOMP, NGPTOTG, GLOBAL_ATLAS_STATE%KFLDX, GLOBAL_ATLAS_STATE%PTSPHY)
+  ENDDO
 
-! Validate the output against serialized reference data
-IF (NITER == 1) CALL GLOBAL_ATLAS_STATE%VALIDATE(FSET, FSPACE, NGPTOTG)
+  ! Validate the output against serialized reference data
+  IF (NITER == 1) CALL GLOBAL_ATLAS_STATE%VALIDATE(FSET, FSPACE, NGPTOTG)
 
-CALL FSET%FINAL()
-CALL FSPACE%FINAL()
-CALL TRACE%FINAL()
+  CALL FSET%FINAL()
+  CALL FSPACE%FINAL()
+  CALL TRACE%FINAL()
 
-! Tear down MPI environment
-CALL ATLAS_LIBRARY%FINALISE()
-CALL CLOUDSC_MPI_END()
+  ! Tear down MPI environment
+  CALL ATLAS_LIBRARY%FINALISE()
+  CALL CLOUDSC_MPI_END()
+
+CONTAINS
+
+  SUBROUTINE GET_ENV_INT(name, value)
+    character(len=*), intent(in):: name
+    INTEGER, intent(inout) :: value
+    character(len=32) :: value_str
+    integer :: value_str_length
+    CALL GET_ENVIRONMENT_VARIABLE(NAME=trim(name), VALUE=value_str, LENGTH=value_str_length)
+    IF (value_str_length > 0 ) THEN
+      READ(value_str(1:value_str_length), *) value
+    ENDIF
+  END SUBROUTINE
 
 END PROGRAM DWARF_CLOUDSC
